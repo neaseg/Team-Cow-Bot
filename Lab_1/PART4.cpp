cd655f50d34d30ee92e50a0b92cc5380cc7007a0
powell759 <powell.759@osu.edu>
2015-02-16 08:07:55 -0500

    Update PART4.cpp

--- Lab_1/PART4.cpp
+++ Lab_1/PART4.cpp
@@ -1,38 +1,21 @@
-/*Written By: Garrett Nease*/
+/*Written By: Maxwell Powell*/
 /*Date Created: 2/13/15*/
 /*Revision: 0*/
 /*Date Revised: 2/13/15*/
-/*Program Function: Read in values of cds cell to create color values.*/
+/*Program Function: Read in values of cds cell to operate motor.*/
 
-#include <FEHLCD.h>
-#include <FEHIO.h>
-#include <FEHUtility.h>
-#include <FEHBUZZER.h>
-#include <FEHMotor.h>
-#include <FEHServo.h>
-
-
-int main(void)
-{
-    /*Initialize all inputs and outpus, as well as motors.*/
-    ButtonBoard buttons( FEHIO::Bank3 );
-    AnalogInputPin cdscell(FEHIO::P1_0);
+int main(void){
+    ButtonBoard buttons(FEHIO::Bank3);
+    AnalogInputPin CDSCell(FEHIO::P1_0);
     FEHMotor motor0 (FEHMotor::Motor0);
-    DigitalInputPin microSwitch (FEHIO::P0_0);
-    /*initialize sensor values*/
-    float cdsvalue;
-    
-    /*Run motor0 at full speed*/
 
-    LCD.Clear( FEHLCD::Black );
-    LCD.SetFontColor( FEHLCD::White );
-    while(1)
-    {
-        /*Read in cdscell value to cdsvalue then print it.*/
-        cdsvalue = cdscell.Value();
-        LCD.Write( cdsvalue );
-        Sleep(250);
+    while(!buttons.MiddlePressed()){
+        Sleep(10);
     }
-            
-    return 0;
+    Sleep(1000);
+    motor0.SetPercent(100);
+    while(!buttons.MiddlePressed()){
+        motor0.SetPercent((1.65 - CDSCell.Value()) * 100 /1.65);
+    }
+    motor0.SetPercent(0);
 }
