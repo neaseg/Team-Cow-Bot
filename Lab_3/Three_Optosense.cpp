8dc01927ee9dc4dc10cf462ca83a5dc6e2e9aa39
powell759 <powell.759@osu.edu>
2015-02-23 09:56:29 -0500

    Update Three_Optosense.cpp

--- Lab_3/Three_Optosense.cpp
+++ Lab_3/Three_Optosense.cpp
@@ -3,14 +3,14 @@
 
 //THREE OPTOSENSOR CODE
 //DEFINE STATES
-#define LEFT_ON_LINE  0;
-#define LEFT_TWO_ON_LINE  1;
-#define MIDDLE_ON_LINE  2;
-#define RIGHT_ON_LINE  3;
-#define RIGHT_TWO_ON_LINE  4;
-#define THRESHOLD  2;
-#define FULL_POWER 50;
-#define HALF_POWER 25;
+#define LEFT_ON_LINE  0
+#define LEFT_TWO_ON_LINE  1
+#define MIDDLE_ON_LINE  2
+#define RIGHT_ON_LINE  3
+#define RIGHT_TWO_ON_LINE  4
+#define THRESHOLD  2
+#define FULL_POWER 50
+#define HALF_POWER 25
 
 //DECLARES OPTOSENSORS
 AnalogInputPin leftOpto(FEHIO::P1_0);
@@ -29,6 +29,9 @@ int main(){
   
   //INFINITE LINE FOLLOWING LOOP
   while(true){
+    
+    //SLEEP
+    Sleep(100);
   
     //GETS OPTOSENSOR VALUES
     leftValue = leftOpto.Value();
@@ -36,17 +39,21 @@ int main(){
     rightValue = rightOpto.Value();
     
     //SETS ROBOT STATE
-    if(leftValue < THRESHOLD && middleValue > THRESHOLD && rightValue > THRESHOLD){
+    if(leftValue > THRESHOLD && middleValue < THRESHOLD && rightValue < THRESHOLD)){
       state = LEFT_ON_LINE;
-      } else if(leftValue < THRESHOLD && middleValue > THRESHOLD && rightValue > THRESHOLD){
+      } else if(leftValue > THRESHOLD && middleValue > THRESHOLD && rightValue < THRESHOLD){
       state = LEFT_TWO_ON_LINE;
-      } else if(leftValue < THRESHOLD && middleValue > THRESHOLD && rightValue > THRESHOLD){
+      } else if(leftValue < THRESHOLD && middleValue > THRESHOLD && rightValue < THRESHOLD){
       state = MIDDLE_ON_LINE;
-      } else if(leftValue < THRESHOLD && middleValue > THRESHOLD && rightValue > THRESHOLD){
+      } else if(leftValue < THRESHOLD && middleValue < THRESHOLD && rightValue > THRESHOLD){
       state = RIGHT_ON_LINE;
       } else if(leftValue < THRESHOLD && middleValue > THRESHOLD && rightValue > THRESHOLD){
       state = RIGHT_TWO_ON_LINE;
       }
+      
+      //WRITES STATE TO SCREEN
+      LCD.Clear();
+      LCD.Write(state);
       
       //ACTS BASED ON STATE
       switch(state){
