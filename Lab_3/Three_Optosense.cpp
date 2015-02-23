c5b3633ba10ed347e0eed86e9d02be2bd507e09d
powell759 <powell.759@osu.edu>
2015-02-23 09:32:10 -0500

    Update Three_Optosense.cpp

--- Lab_3/Three_Optosense.cpp
+++ Lab_3/Three_Optosense.cpp
@@ -9,11 +9,18 @@
 #define RIGHT_ON_LINE  3;
 #define RIGHT_TWO_ON_LINE  4;
 #define THRESHOLD  2;
+#define FULL_POWER 50;
+#define HALF_POWER 25;
 
 //DECLARES OPTOSENSORS
 AnalogInputPin leftOpto(FEHIO::P1_0);
 AnalogInputPin middleOpto(FEHIO::P1_0);
 AnalogInputPin rightOpto(FEHIO::P1_0);
+
+//DECLARES MOTORS
+FEHMotor rightMotor (FEHMotor::Motor0);
+FEHMotor leftMotor (FEHMotor::Motor1);
+
 
 int main(){
 //SETS INITIAL STATE & DECLARES VARIABLES FOR SENSOR VALUES
@@ -40,6 +47,45 @@ int main(){
       } else if(leftValue < THRESHOLD && middleValue > THRESHOLD && rightValue > THRESHOLD){
       state = RIGHT_TWO_ON_LINE;
       }
+      
+      //ACTS BASED ON STATE
+      switch(state){
+        
+        //TURNS LEFT IF ONLY LEFT IS ON LINE
+        case LEFT_ON_LINE;
+          rightMotor.SetPercent(FULL_POWER);
+          leftMotor.SetPercent(HALF_POWER);
+          break;
+          
+        //TURNS LEFT IF ONLY LEFT TWO ARE ON ON LINE
+        case LEFT_TWO_ON_LINE;
+          rightMotor.SetPercent(FULL_POWER);
+          leftMotor.SetPercent(HALF_POWER);
+          break;
+          
+        //NO TURN IF ONLY MIDDLE IS ON LINE
+        case MIDDLE_ON_LINE;
+          rightMotor.SetPercent(FULL_POWER);
+          leftMotor.SetPercent(FULL_POWER);
+          break;
+          
+        //TURNS RIGHT IF ONLY RIGHT IS ON LINE
+        case LEFT_ON_LINE;
+          rightMotor.SetPercent(HALF_POWER);
+          leftMotor.SetPercent(FULL_POWER);
+          break;
+          
+        //TURNS RIGHT IF RIGHT TWO ARE ON LINE
+        case LEFT_ON_LINE;
+          rightMotor.SetPercent(FULL_POWER);
+          leftMotor.SetPercent(HALF_POWER);
+          break;
+          
+        default:
+          break;
+      }
+  }
+}
   
   
 
