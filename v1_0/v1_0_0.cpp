0cfcb8964ba214ef9cfea23be19309c00b83be22
powell759 <powell.759@osu.edu>
2015-03-01 15:18:18 -0500

    Update v1_0_0.cpp

--- v1_0/v1_0_0.cpp
+++ v1_0/v1_0_0.cpp
@@ -12,28 +12,42 @@ FEHMotor left_motor(FEHMotor::Motor1);
 FEHServo servo(FEHServo::Servo7);
 AnalogInputPin cdscell(FEHIO::P1_0);
 
-void start_light()
-{
-//defines percent difference for start light 
-define PERCENT_DIFFERENCE 10;
-float ambientBaseLine = 0;
-for( int i = 0; i < 5; i++){
+void start_light(){
+    
+    //defines percent difference for start light 
+    #define TARGET_DIFF 10;
+    #define LIGHT_SAMPLES 5;
+    float percentDiff;
+    int i;
+    float ambientLightSum = 0;
+    float ambientLightAvg = 0;
 
-for 
-while (!buttons.MiddlePressed())
-{
+    //Gets avg ambient light avetrage over approximately one seccond.
+    for( int i = 0; i < 5; i++){
+        ambientLightSum += cdscell.Value();
+        Sleep(100);
+    }
+    ambientLightAvg = AmbientLightSum / LIGHT_SAMPLES;
+
+    //Waits for button press and depress.
+    while (!buttons.MiddlePressed()){}
+    while (buttons.MiddlePressed()){}
+    
+    //Reads until cdscell.Value()  is more than TARGET_DIFF % greater than ambientLightAvg.
+    do{
+        
+        LCD.Clear();
+        LCD.WriteLine("WAITING FOR START LIGHT");
+        LCD.WriteLine("CURRENT CDS VALUE: " );
+        LCD.Write(cdscell.Value());
+    } while (cdscell.Value() > 3.0);
+    
+    //Writes success message to screen.
+    LCD.Clear();
+    LCD.WriteLine("START LIGHT DETECTED");
 }
-Sleep(1000);
-while (cdscell.Value() > 3.0) //while the cds value shows there is no light on
-{
-LCD.Clear();
-LCD.WriteLine("WAITING FOR START LIGHT");
-LCD.WriteLine("CURRENT CDS VALUE: " );
-LCD.Write(cdscell.Value());
-}
-LCD.Clear();
-LCD.WriteLine("START LIGHT DETECTED");
-}
+
+
 void move_forward(float inches, int percent) //using encoders
 {
 //Reset encoder counts
