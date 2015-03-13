bd677def71851ace85a8658480be685e50b3dc57
Garrett Nease <garrett.nease@gmail.com>
2015-03-13 09:41:26 -0400

    Create Part5.cpp

---
+++ Labs/Lab.1/Part5.cpp
@@ -0,0 +1,32 @@
+/*Written By: Krishna Ganesan*/
+/*Date Created: 2/13/15*/
+/*Revision: 0*/
+/*Date Revised: 2/15/15*/
+/*Program Function: Changes the position of the servo based on the cds cell value.*/
+
+#include <FEHUtility.h>
+#include <FEHServo.h>
+
+// declare servo arm and cdscell
+
+FEHServo servo(FEHServo::Servo7);
+AnalogInputPin cdscell(FEHIO::P1_0);
+int main(void)
+
+{
+
+  // calibrate servo
+ servo.Calibrate();
+ 
+ // sets the values of the minimum and maximum allowed angle
+ //calculated from the calibration process
+ servo.SetMin(998);
+ servo.SetMax(1852);
+ 
+ //while loop that runs indefinitely
+ while (1)
+ {
+   //Changes the angle of the servo based on the value of the cds cell
+ servo.SetAngle(cdscell.Value() * (180/3.3));
+ }
+}
