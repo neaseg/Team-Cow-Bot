9e80efd27b277fee4f28643603c823f987efeee0
powell759 <powell.759@osu.edu>
2015-03-27 08:09:25 -0400

    Update CRANKLIGHTCODE.cpp

--- Main/V4/CRANKLIGHTCODE.cpp
+++ Main/V4/CRANKLIGHTCODE.cpp
@@ -1,3 +1,11 @@
 #define MIDDLEVALUE 1.5
-boolean turnsRight = false
-if( cdscell.Value() > MIDDLEVALE)
+boolean turnsRight = false;
+if( cdscell.Value() > MIDDLEVALE){
+  turnsRight = true;
+}
+
+if(turnsRight){
+  //code to turn right
+} else {
+  //code to turn left
+}
